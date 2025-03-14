c8aa69aab30cc51cda09c2ecea93abdfe6b04fa6
Kenth963 <60262666+Kenth963@users.noreply.github.com>
2025-03-14 20:00:23 +0800

    Add files via upload

---
+++ newone.py
@@ -0,0 +1,51 @@
+from api import Command, Simulation, UP, DOWN, MOVE, STOP
+
+
+def newone():
+    """An example bot that sends elevators up and down and stops at floors if there are passengers waiting to get on or off"""
+    simulation = Simulation(
+        event="secondspace2025",
+        building_name="tiny_random",
+        bot="updown-python-bot",
+        email="bob@mail.com",
+        sandbox=True,
+    )
+    current_state = simulation.initial_state
+    directions = {}  # current directions of elevators
+
+    while current_state["running"]:
+        commands = []
+        for elevator in current_state["elevators"]:
+        
+            direction = directions.get(elevator["floor"], UP)
+
+            directions[elevator["id"]] = direction
+            action = MOVE
+
+            for request in current_state["requests"]:
+                if (
+                    request["floor"] == elevator["floor"]
+                    and request["direction"] == direction
+                ):
+                    action = STOP
+                else:
+                    if elevator["floor"] != request["floor"] and elevator["floor"] > request["floor"]:
+                        direction = DOWN
+                    elif elevator["floor"] != request["floor"] and elevator["floor"] < request["floor"]:
+                        direction = UP
+                    action = MOVE
+
+                    
+
+            commands.append(
+                Command(elevator_id=elevator["id"], direction=direction, action=action)
+            )
+
+        current_state = simulation.send(commands)
+
+    print("Score:", current_state.get("score"))
+    print("Replay URL:", current_state.get("replay_url"))
+
+
+if __name__ == "__main__":
+    newone()
